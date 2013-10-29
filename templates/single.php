1a6dda2e6e7aedfff4dce535fd9e86784f0a1196
Sven von Arx <sven.vonarx@cubetech.ch>
2013-10-29 16:48:38 +0100

    bugfixes..

--- templates/single.php
+++ templates/single.php
@@ -14,13 +14,13 @@
 				if(isset($post_meta_data['cubetech_future_movie']))
 					$youtube = $post_meta_data['cubetech_future_movie'];
 				
-				if($youtube) {
+				if($youtube[0] !== '') {
 					$contentreturn .= '
 					<iframe width="100%" height="100%" src="//www.youtube.com/embed/' . $youtube[0] . '" frameborder="0" allowfullscreen></iframe>';
 				}
 				foreach($post_meta_data as $p) {
 					$image = wp_get_attachment_image($p[0], 'cubetech-future-icon');
-					if ( $image && !$youtube ) {
+					if ( $image && $youtube[0] == '' ) {
 						$contentreturn .= '
 							<li class="cubetech-future-icon cubetech-future-slide-' . $i . '">
 								' . $image . '
